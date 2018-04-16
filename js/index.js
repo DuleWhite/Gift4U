61c69346f6d8593328b397cb38292d2e4b27f414
DuleWhite <dulewhite@gmail.com>
2018-04-16 20:26:08 +0800

    finish product tag, mobile UI fit, Gift Box part

--- js/index.js
+++ js/index.js
@@ -1,16 +1,28 @@
 $(function(){
 	var slick = $('.slick');
 	if($(window).width()>=900){
+		slick.width(900);
 		slick.not('.slick-initialized').slick({
 			dots: false,
 			slidesToShow: 3,
 			slidesToScroll: 1
 		});
 	}
+	else{
+		slick.width($(window).width()-50);
+	}
+	if($(window).width()<940){
+		$(".faq img").width(0.90 * $(window).width())
+	}
 });
 $(window).resize(function(){
-
 	checkSlick();
+	if($(window).width()<940){
+		$(".faq img").width(0.95 * $(window).width() - 50 )
+	}
+	else{
+		slick.width(940);
+	}
 })
 function checkSlick(){
 	var slick = $('.slick');
@@ -19,6 +31,7 @@ function checkSlick(){
 		slick.width($(window).width());
 	}
 	else{
+		slick.width(900);
 		slick.not('.slick-initialized').slick({
 			dots: false,
 			slidesToShow: 3,
