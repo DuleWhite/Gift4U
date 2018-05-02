cb2d13af1e453ba5a60ffbddbf6f5829678928f9
DuleWhite <dulewhite@gmail.com>
2018-05-03 04:07:37 +0800

    finish product part

---
+++ js/productDetails.js
@@ -0,0 +1,63 @@
+$(function(){
+	// magicZoom
+    $("#zoom-img").magicZoom();
+    var currentSelectedImg = $(".guide-img")[0];
+    $(".guide-img").click(function(){
+        var oldPath = this.src, newPath = "";
+        var oldPaths = oldPath.split(".");
+        for (var i = 0; i < oldPaths.length - 1; i++) { 
+        	if(newPath!=""){
+        		newPath+=".";
+        	}
+        	newPath += oldPaths[i];
+        }
+        newPath += '-L.'; newPath += oldPaths[oldPaths.length-1];
+        $("#zoom-img").attr('src',newPath);
+        $("#magiczoom-zoom").css('background-image','url('+newPath+')');
+        $(currentSelectedImg).removeClass("selected");
+        $(this).addClass("selected");
+        currentSelectedImg = this;
+    });
+
+    var currentSelectedColor = null
+    $(".color-label").click(function(){
+    	if(currentSelectedColor){
+    		$(currentSelectedColor).removeClass("selected");
+    	}
+    	$(this).addClass("selected");
+    	currentSelectedColor = this;
+    });
+
+    document.getElementById("quantity-input").addEventListener("keypress",function(event){
+        if(event.keyCode == 45){
+            event.preventDefault();
+        }
+    });
+
+    $("#quantity-input").val("1");
+
+    var product_parameters = $(".product-parameters li button");
+
+    for(var i = 0 ; 
+        i < product_parameters.length ; 
+        i++ ){
+        var h3title = $(product_parameters[i]).find("h3");
+        if(h3title.attr("class") == "on"){
+            $(product_parameters[i]).next().css("display","block");
+        }
+    }
+
+    var currentOnProductPatameters = product_parameters[0];
+    product_parameters.click(function(){
+        var h3title = $(this).find("h3");
+        if(h3title.attr("class")=="off"){
+            $(currentOnProductPatameters).next().css("display","none");
+            $(currentOnProductPatameters).find("h3").removeClass("on");
+            $(currentOnProductPatameters).find("h3").addClass("off");
+            $(this).next().css("display","block");
+            h3title.removeClass("off");
+            h3title.addClass("on");
+            currentOnProductPatameters = this;
+        }
+    });
+});
